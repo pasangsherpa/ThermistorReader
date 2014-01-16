4aa42f9371aabe89b0236cc38c861adaddaf32a1
pasang sherpa <pasang_sh@hotmail.com>
2014-01-16 12:03:22 -0800

    initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 15, 2013
+ *
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int test() {
+	printf("Hello World");
+	return 0;
+}
+
+int main(void) {
+	test();
+	return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: Jan 16, 2014
+ *
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#endif /* MAIN_H_ */
