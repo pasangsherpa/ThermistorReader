30aa2411ba5b9d66d0688b95eb90a6969a00bac9
aaronsmooth <anelson7@uw.edu>
2014-01-23 14:56:32 -0800

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,144 @@
+/*
+ *  FILE   : queue.c
+ *  AUTHOR : Jeffrey Hunter
+ *  WEB    : http://www.iDevelopment.info
+ *  NOTES  : Implement all functions required
+ *           for a Queue data structure.
+ */
+
+#include "queue.h"
+#include <stdlib.h>
+
+#define MinQueueSize (5)
+
+struct QueueRecord {
+  int Capacity;
+  int Front;
+  int Rear;
+  int Size;
+  ElementType *Array;
+};
+
+int IsEmpty(Queue Q) {
+  return Q->Size == 0;
+}
+
+int IsFull(Queue Q) {
+  return Q->Size == Q->Capacity;
+}
+
+int Size(Queue Q) {
+  return Q->Size;
+}
+
+Queue CreateQueue(int MaxElements) {
+  Queue Q;
+
+  if (MaxElements < MinQueueSize) {
+    Error("CreateQueue Error: Queue size is too small.");
+  }
+
+  Q = malloc (sizeof(struct QueueRecord));
+  if (Q == NULL) {
+    FatalError("CreateQueue Error: Unable to allocate more memory.");
+  }
+
+  Q->Array = malloc( sizeof(ElementType) * MaxElements );
+  if (Q->Array == NULL) {
+    FatalError("CreateQueue Error: Unable to allocate more memory.");
+  }
+
+  Q->Capacity = MaxElements;
+  MakeEmpty(Q);
+
+  return Q;
+}
+
+void MakeEmpty(Queue Q) {
+
+  Q->Size = 0;
+  Q->Front = 1;
+  Q->Rear = 0;
+
+}
+
+void DisposeQueue(Queue Q) {
+  if (Q != NULL) {
+    free(Q->Array);
+    free(Q);
+  }
+}
+
+static int Succ(int Value, Queue Q) {
+  if (++Value == Q->Capacity) {
+    Value = 0;
+  }
+  return Value;
+}
+
+void Enqueue(ElementType X, Queue Q) {
+
+  if (IsFull(Q)) {
+    Error("Enqueue Error: The queue is full.");
+  } else {
+    Q->Size++;
+    Q->Rear = Succ(Q->Rear, Q);
+    Q->Array[Q->Rear] = X;
+  }
+
+}
+
+ElementType Front(Queue Q) {
+
+  if (!IsEmpty(Q)) {
+    return Q->Array[Q->Front];
+  }
+  Error("Front Error: The queue is empty.");
+
+  /* Return value to avoid warnings from the compiler */
+  return 0;
+
+}
+
+void Dequeue(Queue Q) {
+
+  if (IsEmpty(Q)) {
+    Error("Dequeue Error: The queue is empty.");
+  } else {
+    Q->Size--;
+    Q->Front = Succ(Q->Front, Q);
+  }
+
+}
+
+ElementType FrontAndDequeue(Queue Q) {
+
+  ElementType X = 0;
+
+  if (IsEmpty(Q)) {
+    Error("FrontAndDequeue Error: The queue is empty.");
+  } else {
+    Q->Size--;
+    X = Q->Array[Q->Front];
+    Q->Front = Succ(Q->Front, Q);
+  }
+  return X;
+
+}
+
+ElementType * getQueue(Queue Q){
+
+        ElementType * result = malloc( sizeof(ElementType) * Q->Size); //returns a mutable copy of the elements array        
+        if(IsEmpty(Q)){
+                Error("getQueue Error: The queue is empty.");
+                return Q->Array;
+        } else {        
+                int index = 0;        
+                int i;
+                for(i = Q->Front; i <= Q->Rear;i++){
+                        result[index] = Q->Array[i];                                
+                        index++;
+                }
+        }
+        return result;
+}
